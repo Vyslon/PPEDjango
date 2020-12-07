3985eb0d4fa225f626336e8fe5487bdf800c3062
Thibault <Thibault@DESKTOP-CE08T5Q>
2020-12-07 16:47:39 +0100

    Test accueil redirige vers login

--- ppefrais/tests.py
+++ ppefrais/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
-from ppefrais.models import Utilisateur
 from django.urls import reverse
 from django.contrib import auth
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+CustomUser = get_user_model()
 
 # HTTP Codes tests :
 
@@ -10,13 +12,11 @@ from django.contrib import auth
 class IndexPageTestCase(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create_user('temporary',
-                                             'temporary@gmail.com', 'temporary')
-        Utilisateur.objects.create(user=self.user, statut="Visiteur", adresse="Adresse")
+        self.user = CustomUser.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
 
-    def test_index_page(self):
-        response = self.client.get(reverse('ppefrais:index'))
-        self.assertEqual(response.status_code, 200)
+    def test_index_page_not_connected_redirection(self):
+        response = self.client.get(reverse('accueil'))
+        self.assertEqual(response.status_code, 302)
 
     def test_index_page_connected(self):
         self.client.login(username='temporary', password='temporary')
@@ -29,7 +29,7 @@ class HomePageTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user('temporary',
                                              'temporary@gmail.com', 'temporary')
-        Utilisateur.objects.create(user=self.user, statut="Visiteur", adresse="Adresse")
+        CustomUser.objects.create(user=self.user, statut="Visiteur", adresse="Adresse")
 
     def test_home_page(self):
         response = self.client.get(reverse('ppefrais:home'))
--- ppefrais/urls.py
+++ ppefrais/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
+
 ]

50a614294409dbe415ac13946df5205b9d7bc7bf
Thibault <Thibault@DESKTOP-CE08T5Q>
2020-12-07 17:29:51 +0100

    Suppression imports inutiles

--- ppefrais/tests.py
+++ ppefrais/tests.py
@@ -1,5 +1,4 @@
-from django.test import TestCase, Client
-from django.contrib.auth.models import User
+from django.test import TestCase
 from django.urls import reverse
 from django.contrib import auth
 from django.contrib.auth import get_user_model

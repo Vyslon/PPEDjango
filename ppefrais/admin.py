f9a6eae15e085db79e3062308518b4c8e47a3234
Thibault <Thibault@DESKTOP-CE08T5Q>
2020-12-09 15:32:26 +0100

    Les champs personnalisés de l'utilisateur apparaissent sur le formulaire d'édition

--- ppefrais/admin.py
+++ ppefrais/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
 from .models import CustomUser
 
 
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'date_embauche')
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
 
 
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('statut', 'adresse', 'code_postal', 'date_embauche',)}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)

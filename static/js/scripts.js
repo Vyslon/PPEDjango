c2a64e8f955f756f1c4b1e3ce4c2f5613e42a42a
Thibault <Thibault@DESKTOP-CE08T5Q>
2020-12-09 16:56:43 +0100

    JS: après clic, élément de navbar perd le focus

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,26 +1,38 @@
-// BURGER MENU
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    // Le navbar-item / navbar-link perd le focus après clic
+    const $navbarItem = Array.prototype.slice.call(document.querySelectorAll(".navbar-item, .navbar-link"), 0);
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
+    if ($navbarItem.length > 0) {
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
+        $navbarItem.forEach(el => {
+            el.addEventListener('click', () => {
+                el.blur();
+            });
+        });
+    }
 
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
+    // BURGER MENU
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
 
-      });
-    });
-  }
+        // Add a click event on each of them
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
 
 });

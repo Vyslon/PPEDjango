645949fda3fed28ce635a4fa3cd692f5be2cc3ee
Thibault <Thibault@DESKTOP-CE08T5Q>
2020-12-09 15:06:50 +0100

    Correction de l'ajout d'un utilisateur

--- ppefrais/admin.py
+++ ppefrais/admin.py
@@ -1,17 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
-
-
-# class CustomUserAdmin(UserAdmin):
-#     form = CustomUserChangeForm
-#     add_form = CustomUserCreationForm
 
 
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'date_embauche')
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser, UserAdmin)

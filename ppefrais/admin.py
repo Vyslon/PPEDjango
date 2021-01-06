602a5cc14a52ba92b48b08d58a03d9e40621b2ab
Thibault <Thibault@DESKTOP-CE08T5Q>
2021-01-06 15:22:36 +0100

    admin.py : ajout LigneFraisForfait

--- ppefrais/admin.py
+++ ppefrais/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm
-from .models import CustomUser, FicheFrais, FraisForfait, LigneFraisHorsForfait, Etat
+from .models import CustomUser, FicheFrais, FraisForfait, LigneFraisHorsForfait, LigneFraisForfait, Etat
 
 
 class CustomUserChangeForm(UserChangeForm):
@@ -20,5 +20,6 @@ class CustomUserAdmin(UserAdmin):
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(FicheFrais),
 admin.site.register(LigneFraisHorsForfait),
+admin.site.register(LigneFraisForfait),
 admin.site.register(FraisForfait),
-admin.site.register(Etat)
+admin.site.register(Etat),

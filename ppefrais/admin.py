57d8cdd0f297fdd64b25762bed9f7dd25d4e82d9
Thibault <Thibault@DESKTOP-CE08T5Q>
2021-01-06 15:14:30 +0100

    admin.py : ajout du modèle FicheFrais

--- ppefrais/admin.py
+++ ppefrais/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm
-from .models import CustomUser
+from .models import CustomUser, FicheFrais
 
 
 class CustomUserChangeForm(UserChangeForm):
@@ -18,3 +18,4 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(FicheFrais)

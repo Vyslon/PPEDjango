16a0a6646bc337ab3274036f1aac784e1068b5c5
Thibault Thomas <thibaulthomas@gmail.com>
2021-01-09 10:50:42 +0100

    urls.py : suppression du chemin saisir-frais renommage des autres chemins

--- ppefrais/urls.py
+++ ppefrais/urls.py
@@ -3,7 +3,6 @@ from . import views
 from .views import FicheCreate
 
 urlpatterns = [
-    path('ficheFrais', views.fiches_frais, name='les-fiches'),
-    path('ficheFrais/<str:moisAnnee>/', views.une_fiche_frais, name='une-fiche'),
-    path('saisir-frais/', views.saisir_frais, name='saisir-frais'),
+    path('fiches-frais/', views.fiches_frais, name='les-fiches'),
+    path('fiches-frais/<str:moisAnnee>/', views.une_fiche_frais, name='une-fiche'),
 ]

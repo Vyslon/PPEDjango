315ace94726cb95edab4f43ba85dce1e004c7900
Thibault Thomas <thibaulthomas@gmail.com>
2021-01-09 11:24:22 +0100

    urls.py : chemins pour la création de lignes de frais

--- ppefrais/urls.py
+++ ppefrais/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import FicheCreate
 
 urlpatterns = [
     path('fiches-frais/', views.fiches_frais, name='les-fiches'),
+    path('fiches-frais/saisie-frais-forfaitise/', views.LigneFraisForfaitiseCreate.as_view, name='ligne-forfait'),
+    path('fiches-frais/saisie-frais-hors-forfait/', views.LigneFraisHorsForfaitCreate.as_view, name='ligne-hors-forfait'),
     path('fiches-frais/<str:moisAnnee>/', views.une_fiche_frais, name='une-fiche'),
 ]

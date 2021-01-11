9b964c281c13f3d789dc58da420f72bd40c88e06
Thibault <Thibault@DESKTOP-CE08T5Q>
2021-01-11 13:58:58 +0100

    "Frais forfaitaire" au lieu de frais forfaitisé

--- ppefrais/urls.py
+++ ppefrais/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('fiches-frais/', views.fiches_frais, name='les-fiches'),
-    path('fiches-frais/<str:mois>/saisie-frais-forfaitise/', views.LigneFraisForfaitCreate.as_view(), name='saisie-ligne-forfait'),
+    path('fiches-frais/<str:mois>/saisie-frais-forfaitaire/', views.LigneFraisForfaitCreate.as_view(), name='saisie-ligne-forfait'),
     path('fiches-frais/<str:mois>/saisie-frais-hors-forfait/', views.LigneFraisHorsForfaitCreate.as_view(), name='saisie-ligne-hors-forfait'),
-    path('fiches-frais/<str:mois>/edition-frais-forfaitise/<int:pk>/', views.LigneFraisForfaitUpdate.as_view(), name='edit-ligne-forfait'),
+    path('fiches-frais/<str:mois>/edition-frais-forfaitaire/<int:pk>/', views.LigneFraisForfaitUpdate.as_view(), name='edit-ligne-forfait'),
     path('fiches-frais/<str:mois>/edition-frais-hors-forfait/<int:pk>/', views.LigneFraisHorsForfaitUpdate.as_view(), name='edit-ligne-hors-forfait'),
     path('fiches-frais/<str:mois>/suprression-frais-hors-forfait/<int:pk>/', views.LigneFraisHorsForfaitDelete.as_view(), name='suppr-ligne-hors-forfait'),
     path('fiches-frais/<str:mois>/', views.une_fiche_frais, name='une-fiche'),

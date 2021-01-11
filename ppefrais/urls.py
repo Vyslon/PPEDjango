0cdfc57c89c4cc65a80d7efab247eecd9977790e
Thibault <Thibault@DESKTOP-CE08T5Q>
2021-01-11 14:34:59 +0100

    URLs : suppr° du chemin nommé "saisie-ligne-forfait"

--- ppefrais/urls.py
+++ ppefrais/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('fiches-frais/', views.fiches_frais, name='les-fiches'),
-    path('fiches-frais/<str:mois>/saisie-frais-forfaitaire/', views.LigneFraisForfaitCreate.as_view(), name='saisie-ligne-forfait'),
     path('fiches-frais/<str:mois>/saisie-frais-hors-forfait/', views.LigneFraisHorsForfaitCreate.as_view(), name='saisie-ligne-hors-forfait'),
     path('fiches-frais/<str:mois>/edition-frais-forfaitaire/<int:pk>/', views.LigneFraisForfaitUpdate.as_view(), name='edit-ligne-forfait'),
     path('fiches-frais/<str:mois>/edition-frais-hors-forfait/<int:pk>/', views.LigneFraisHorsForfaitUpdate.as_view(), name='edit-ligne-hors-forfait'),

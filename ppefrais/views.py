49628e575a90e524cd6f43d863693a3ebd364eff
Thibault <Thibault@DESKTOP-CE08T5Q>
2021-01-16 12:30:19 +0100

    vue pour le pdf

--- ppefrais/views.py
+++ ppefrais/views.py
@@ -1,14 +1,84 @@
+from django.contrib.staticfiles import finders
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.urls import reverse
 
 from .models import FicheFrais, Visiteur, LigneFraisForfait, LigneFraisHorsForfait
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 import datetime
 
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+import os
+from django.conf import settings
+
+
+def une_fiche_frais_pdf(request, mois):
+    template_path = 'ficheFraisPdf.html'
+    usr = request.user
+    try:
+        ficheFrais = FicheFrais.objects.get(mois=mois, visiteur=usr)
+    except:
+        raise Http404("Pas de fiche de frais correspondante")
+
+    lignesFrais = LigneFraisForfait.objects.filter(fiche=ficheFrais)
+    lignesFraisHF = LigneFraisHorsForfait.objects.filter(fiche=ficheFrais)
+
+    context = {
+        'ficheFrais': ficheFrais,
+        'lignesFraisForfait': lignesFrais,
+        'lignesFraisHorsForfait': lignesFraisHF
+    }
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response, link_callback=link_callback)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
+
+
+def link_callback(uri, rel):
+    """
+    Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+    resources
+    """
+    result = finders.find(uri)
+
+    if result:
+        if not isinstance(result, (list, tuple)):
+            result = [result]
+        result = list(os.path.realpath(path) for path in result)
+        path = result[0]
+    else:
+        sUrl = settings.STATIC_URL  # Typically /static/
+        sRoot = settings.STATIC_ROOT  # Typically /home/userX/project_static/
+        mUrl = settings.MEDIA_URL  # Typically /media/
+        mRoot = settings.MEDIA_ROOT  # Typically /home/userX/project_static/media/
+
+        if uri.startswith(mUrl):
+            path = os.path.join(mRoot, uri.replace(mUrl, ""))
+        elif uri.startswith(sUrl):
+            path = os.path.join(sRoot, uri.replace(sUrl, ""))
+        else:
+            return uri
+
+    # make sure that file exists
+    if not os.path.isfile(path):
+        raise Exception(
+            'media URI must start with %s or %s' % (sUrl, mUrl)
+        )
+    return path
+
 
 def fiches_frais(request):
-    usr = Visiteur.objects.filter(id=request.user.id)[0]
     moisEntier = [
         'None',
         'Janvier',
@@ -24,6 +94,7 @@ def fiches_frais(request):
         'Novembre',
         'Décembre'
     ]
+    usr = Visiteur.objects.filter(id=request.user.id)[0]
 
     dateMinimum = str(datetime.datetime.now().year - 1) + '01'
     ficheFrais = FicheFrais.objects.filter(visiteur=usr).order_by('mois').extra(where=['mois>=%s'], params=[dateMinimum])
